51afd067fc05656b58e58fb380786f193617e5bb
Fangchen Li <fangchenli@fangchens-new-macbook-pro.local>
2017-05-17 17:27:33 +0800

    Initial Commit

---
+++ MineSweeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MineSweeper
+//
+//  Created by Fangchen Li on 5/17/17.
+//  Copyright © 2017 Fangchen Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
